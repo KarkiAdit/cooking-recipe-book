72f8d9a26a8ee07fc2dfc00bb1140be1ad686999
KarkiAdit <adityakarki728@gmail.com>
2025-12-04 14:19:55 -0600

    added UI for Home and Recipe pages

---
+++ CookBook/Components/CapsuleTextFieldStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CapsuleTextFieldStyle.swift
+//  CookBook
+//
+//  Created by Aditya Karki on 12/4/25.
+//
+
+import SwiftUI
+
+struct CapsuleTextFieldStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapsuleTextFieldStyle()
+}
